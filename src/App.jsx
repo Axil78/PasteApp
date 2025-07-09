acce682f89c3c1df9c04d8253342abf922353868
Axil78 <142527230+Axil78@users.noreply.github.com>
2025-07-09 17:56:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Paste from './components/Paste'
-import ViewPaste from './components/ViewPaste'
+import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
+import Paste from './components/Paste.jsx'
+import ViewPaste from './components/ViewPaste.jsx'
 
 
 const router = createBrowserRouter([
